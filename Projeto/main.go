be7c5fecc388a809c3ccbddb11f71f9421f78f1c
Bernardo <bemascarenhas6@gmail.com>
2024-04-15 22:44:41 -0300

    final

--- Projeto/main.go
+++ Projeto/main.go
@@ -2,43 +2,56 @@ package main
 
 import (
 	//"Projeto/modelos/pedido"
+	"Projeto/modelos/pedido"
 	"Projeto/modelos/produto"
 	"fmt"
 )
 
-func main(){
-	lista := produto.ListaProdutos{}
-	fmt.Println(lista)
-	lista.Adicionar("Batata", "Frita", 20.0)
-	fmt.Println(lista)
-	lista.Adicionar("Batata", "Cozida", 25.0)
-	fmt.Println(lista)
+func main() {
+	pedidos_lista := pedido.FilaPedidos{}
+	produtos := produto.ListaProdutos{}
+	produtos.Adicionar("batata", "frita", 20.00)
+	produtos.Adicionar("ham", "burguer", 20.00)
+	pedidos_lista.Adicionar(true, produtos.Produtos, 40.00)
+	pedidos_lista.Remover()
+	pedidos_lista.Adicionar(true,produtos.Produtos,30.00)
+	pedidos_lista.ListarPedidos()
+	fmt.Println("\nfim")
 
-	err := lista.Remover(1)
-	fmt.Println(lista)
-	fmt.Println(err)
-	err = lista.Remover(4)
-	fmt.Println(lista)
-	fmt.Println(err)
+	/*
+			lista := produto.ListaProdutos{}
+			fmt.Println(lista)
+			lista.Adicionar("Batata", "Frita", 20.0)
+			fmt.Println(lista)
+			lista.Adicionar("Batata", "Cozida", 25.0)
+			fmt.Println(lista)
 
-	produtoBuscado1, err := lista.BuscarPorId(2)
-	fmt.Println(produtoBuscado1)
-	fmt.Println(err)
+			err := lista.Remover(1)
+			fmt.Println(lista)
+			fmt.Println(err)
+			err = lista.Remover(4)
+			fmt.Println(lista)
+			fmt.Println(err)
 
-	produtoBuscado2, err := lista.BuscarPorId(4)
-	fmt.Println(produtoBuscado2)
-	fmt.Println(err)
+			produtoBuscado1, err := lista.BuscarPorId(2)
+			fmt.Println(produtoBuscado1)
+			fmt.Println(err)
 
-	lista.Adicionar("Mandioca", "Frita", 27.0)
-	lista.Adicionar("Banana", "Frita", 15.0)
-	lista.Adicionar("Batata Doce", "Frita", 32.0)
-	lista.ListarProdutos()
+			produtoBuscado2, err := lista.BuscarPorId(4)
+			fmt.Println(produtoBuscado2)
+			fmt.Println(err)
 
-	/*fila := pedido.FilaPedidos{}
-	fila.Adicionar(true, lista.Produtos, 15.0)
+			lista.Adicionar("Mandioca", "Frita", 27.0)
+			lista.Adicionar("Banana", "Frita", 15.0)
+			lista.Adicionar("Batata Doce", "Frita", 32.0)
+			lista.ListarProdutos()
 
-    fmt.Println("Fila de Pedidos:")
-    for _, pedido := range fila.Pedidos {
-        fmt.Printf("ID: %d, Delivery: %t, Valor Total: R$ %.2f\n", pedido.Id, pedido.Delivery, pedido.ValorTotal)
-    }*/ 
+			fila := pedido.FilaPedidos{}
+			fila.Adicionar(true, lista.Produtos, 15.0)
+
+		    fmt.Println("Fila de Pedidos:")
+		    for _, pedido := range fila.Pedidos {
+		        fmt.Printf("ID: %d, Delivery: %t, Valor Total: R$ %.2f\n", pedido.Id, pedido.Delivery, pedido.ValorTotal)
+		    }*/
+
 }
--- Projeto/modelos/pedido/filaPedidos.go
+++ Projeto/modelos/pedido/filaPedidos.go
@@ -1,6 +1,9 @@
 package pedido
 
-import "Projeto/modelos/produto"
+import (
+	"Projeto/modelos/produto"
+	"fmt"
+)
 
 type FilaPedidos struct {
 	Pedidos []Pedido
@@ -14,4 +17,24 @@ func (f *FilaPedidos) Adicionar(delivery bool, produtos []produto.Produto, valor
 	}
 	p.criaID()
 	f.Pedidos = append(f.Pedidos, p)
+}
+func (f *FilaPedidos) Remover() {
+    if len(f.Pedidos) > 0 {
+        f.Pedidos = f.Pedidos[1:]
+    }
+}
+
+
+
+func (f *FilaPedidos) ListarPedidos(){
+
+	for _, pedido := range f.Pedidos {
+        fmt.Printf("ID: %d, Delivery: %t, Valor: R$ %.2f\n", pedido.Id, pedido.Delivery, pedido.ValorTotal)
+        fmt.Println("Produtos:")
+        for _, produto := range pedido.Produtos {
+            fmt.Printf("Nome: %s, Descrição: %s, Valor: R$ %.2f\n", produto.Nome, produto.Descricao, produto.Valor)
+        }
+        fmt.Println("------------")
+    }
+
 }
