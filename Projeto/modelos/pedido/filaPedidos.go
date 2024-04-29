ae87ad5d02327b4e4d82c2b28f9c9e27ebc76505
Bernardo <bemascarenhas6@gmail.com>
2024-04-29 17:10:34 -0300

    amem

--- Projeto/modelos/pedido/filaPedidos.go
+++ Projeto/modelos/pedido/filaPedidos.go
@@ -10,22 +10,36 @@ type FilaPedidos struct {
 
 var FPedidos FilaPedidos
 
-func (f *FilaPedidos) Adicionar(delivery bool, produtos []produto.Produto, valorTotal float64){
+func (f *FilaPedidos) Adicionar(delivery bool, produtosIDs []int) error {
+	var produtos []produto.Produto
+	var valorTotal float64
+
+	for _, id := range produtosIDs {
+		prod, err := produto.LProdutos.BuscarPorId(id)
+		if err != nil {
+			return err // Se houver um erro, retorne imediatamente
+		}
+		produtos = append(produtos, *prod)
+		valorTotal += prod.Valor
+	}
+
 	p := Pedido{
-		Delivery: delivery,
-		Produtos: produtos,
+		Delivery:   delivery,
+		Produtos:   produtos,
 		ValorTotal: valorTotal,
 	}
 	p.criaID()
 	f.Pedidos = append(f.Pedidos, p)
+
+	return nil
 }
 
 func (f *FilaPedidos) Expedir() {
-    if len(f.Pedidos) > 0 {
-        f.Pedidos = f.Pedidos[1:]
-    }
+	if len(f.Pedidos) > 0 {
+		f.Pedidos = f.Pedidos[1:]
+	}
 }
 
-func (f *FilaPedidos) ListarPedidos() []Pedido{
+func (f *FilaPedidos) ListarPedidos() []Pedido {
 	return f.Pedidos
 }
